dae67e1f2dfb15886d81aef0ecbfbc184269dd9d
Hadi <abbassyyt@gmail.com>
2025-11-09 12:25:43 -0500

    Initial Commit

---
+++ FitTrack Code/FitTrack_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrack_CodeApp.swift
+//  FitTrack Code
+//
+//  Created by Hadi Abbas on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FitTrack_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
